1ef06d044eef3893c2bec75c69d57db2b1a3f773
jaimetalaverabermejo <jaime.talavera.bermejo@gmail.com>
2026-09-13 20:18:16 +0200

    Assemble v3 embedded prototype assets

---
+++ src/assets/embeddedAssets.ts
@@ -0,0 +1,19 @@
+// Generated prototype assets. Replace with normal public files later.
+import bandle0 from './chunks/bandle_0';
+import bandle1 from './chunks/bandle_1';
+import bandle2 from './chunks/bandle_2';
+import garenWorld0 from './chunks/garenWorld_0';
+import garenWorld1 from './chunks/garenWorld_1';
+import garenWorld2 from './chunks/garenWorld_2';
+import garenWorld3 from './chunks/garenWorld_3';
+import garenBattle0 from './chunks/garenBattle_0';
+import garenBattle1 from './chunks/garenBattle_1';
+import teemoBattle0 from './chunks/teemoBattle_0';
+import teemoBattle1 from './chunks/teemoBattle_1';
+
+export const GAREN_OVERWORLD_FRAME_WIDTH = 30;
+export const GAREN_OVERWORLD_FRAME_HEIGHT = 30;
+export const BANDLE_MAP_DATA_URI = 'data:image/webp;base64,' + bandle0 + bandle1 + bandle2;
+export const GAREN_OVERWORLD_SHEET_DATA_URI = 'data:image/png;base64,' + garenWorld0 + garenWorld1 + garenWorld2 + garenWorld3;
+export const GAREN_BATTLE_BACK_DATA_URI = 'data:image/png;base64,' + garenBattle0 + garenBattle1;
+export const TEEMO_BATTLE_FRONT_DATA_URI = 'data:image/png;base64,' + teemoBattle0 + teemoBattle1;
