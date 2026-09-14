eb821321413262bb3ab5455ea4d6534e4cba5e92
jaimetalaverabermejo <jaime.talavera.bermejo@gmail.com>
2026-09-14 11:51:14 +0200

    feat: preload Bandle Village

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -16,23 +16,23 @@ export class BootScene extends Phaser.Scene {
 
     this.load.svg('garen-battle-back', './assets/champions/garen/battle/back.svg');
     this.load.image('teemo-battle-front', './assets/champions/teemo/battle/front.png');
-
-    this.load.image(
-      'bandle-bg',
-      './assets/world/regions/bandle-city/zones/portal-clearing/overworld.png'
-    );
+    this.load.image('bandle-bg', './assets/world/regions/bandle-city/zones/portal-clearing/overworld.png');
+    this.load.svg('bandle-village-bg', './assets/world/regions/bandle-city/zones/bandle-village/overworld.svg', { width: 1024, height: 768 });
   }
 
   create(): void {
     DataRegistry.champion('garen');
     DataRegistry.champion('teemo');
     DataRegistry.map('bandle-debug');
+    DataRegistry.map('bandle-village');
+    DataRegistry.map('bandle-house-01');
     DataRegistry.encounter('bandle-meadow');
 
     this.textures.get('garen-overworld').setFilter(Phaser.Textures.FilterMode.NEAREST);
     this.textures.get('garen-battle-back').setFilter(Phaser.Textures.FilterMode.NEAREST);
     this.textures.get('teemo-battle-front').setFilter(Phaser.Textures.FilterMode.NEAREST);
     this.textures.get('bandle-bg').setFilter(Phaser.Textures.FilterMode.NEAREST);
+    this.textures.get('bandle-village-bg').setFilter(Phaser.Textures.FilterMode.NEAREST);
 
     const save = SaveService.load();
     const map = DataRegistry.map(save.currentMapId);
