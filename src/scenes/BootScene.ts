220f7e89d92f30f6398611e55f7323ac790a7a36
jaimetalaverabermejo <jaime.talavera.bermejo@gmail.com>
2026-09-16 17:12:51 +0200

    fix(v16.1): load UI960 atlas as image and register frames explicitly

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -4,6 +4,8 @@ import { DataRegistry } from '../data/DataRegistry';
 import { SaveService } from '../systems/save/SaveService';
 import { V15TestRosterService } from '../systems/testing/V15TestRosterService';
 import { BATTLE_UI_ATLAS_DATA_URI, BATTLE_UI_FRAMES } from '../ui/battle/v2/assets';
+import { BATTLE_UI_960_FRAMES } from '../ui/battle/v3/assets';
+import { START_FRAMES_960 } from '../ui/battle/v3/frames-start';
 import { LEGACY_ASSET_STANDARD } from '../config/AssetStandards';
 
 export class BootScene extends Phaser.Scene {
@@ -41,7 +43,7 @@ export class BootScene extends Phaser.Scene {
     this.load.image('item-speed-legendary', './assets/items/legendary/speed-legendary.png');
 
     this.load.image('battle-ui-v2', BATTLE_UI_ATLAS_DATA_URI);
-    this.load.atlas('battle-ui-960', './assets/ui/ui_960_v1/ui960-atlas.png', './assets/ui/ui_960_v1/ui960-atlas.json');
+    this.load.image('battle-ui-960', './assets/ui/ui_960_v1/ui960-atlas.png');
     this.load.image('bandle-bg', './assets/world/regions/bandle-city/zones/portal-clearing/overworld.png');
     this.load.svg('bandle-village-bg', './assets/world/regions/bandle-city/zones/bandle-village/overworld.svg', { width: 1024, height: 768 });
   }
@@ -88,10 +90,15 @@ export class BootScene extends Phaser.Scene {
     }
 
     this.textures.get('battle-ui-v2').setFilter(Phaser.Textures.FilterMode.NEAREST);
-    this.textures.get('battle-ui-960').setFilter(Phaser.Textures.FilterMode.NEAREST);
     const battleUiTexture = this.textures.get('battle-ui-v2');
     for (const [frameName, frame] of Object.entries(BATTLE_UI_FRAMES)) {
       battleUiTexture.add(frameName, 0, frame.x, frame.y, frame.w, frame.h);
+    }
+
+    const ui960 = this.textures.get('battle-ui-960');
+    ui960.setFilter(Phaser.Textures.FilterMode.NEAREST);
+    for (const [frameName, frame] of Object.entries({ ...BATTLE_UI_960_FRAMES, ...START_FRAMES_960 })) {
+      if (!ui960.has(frameName)) ui960.add(frameName, 0, frame.x, frame.y, frame.w, frame.h);
     }
 
     this.textures.get('bandle-bg').setFilter(Phaser.Textures.FilterMode.NEAREST);
@@ -115,7 +122,7 @@ export class BootScene extends Phaser.Scene {
       localStorage.setItem(v15TestRosterKey, 'done');
     }
 
-    this.registry.set('app.version', '16.1 UI960 TEST');
+    this.registry.set('app.version', '16.1.1 UI960 FIX');
     this.registry.set('save', save);
     this.scene.start('TitleScene');
   }
