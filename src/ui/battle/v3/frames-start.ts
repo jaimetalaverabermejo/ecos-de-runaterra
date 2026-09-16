95eb294ff5f418c80ed75a4720bba6f763ac0903
jaimetalaverabermejo <jaime.talavera.bermejo@gmail.com>
2026-09-16 17:11:57 +0200

    feat(v16.1): add start frames to UI960 map

---
+++ src/ui/battle/v3/frames-start.ts
@@ -0,0 +1,5 @@
+export const START_FRAMES_960 = {
+  '57_title_logo.png': { x: 434, y: 303, w: 520, h: 150 },
+  '58_save_slot_panel.png': { x: 2, y: 455, w: 760, h: 120 },
+  '59_save_option_panel.png': { x: 2, y: 577, w: 320, h: 120 }
+} as const;
